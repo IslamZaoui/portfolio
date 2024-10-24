eae9f39c1052ad866e9264307eee4bcf6c8fe318
Islam Zaoui <98623858+IslamZaoui@users.noreply.github.com>
2024-10-25 00:15:09 +0100

    fix: og image url again

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -3,7 +3,7 @@ import * as runtime from '@/paraglide/runtime.js';
 
 export const i18n = createI18n(runtime, {
 	prefixDefaultLanguage: 'always',
-	exclude: ['/sitemap.xml', /^\/OG\/.*/, '/robots.txt', /^\/assets\/.*/, /^\/config\/.*/],
+	exclude: ['/sitemap.xml', '/robots.txt', /^\/assets\/.*/, /^\/config\/.*/, /^\/api\/og\/.*/],
 	textDirection: {
 		ar: 'rtl',
 		en: 'ltr'
