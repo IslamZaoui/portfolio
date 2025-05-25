5c2fc7e84896dd69001573f05946053cc742ac2a
IslamZaoui <zaoui.islam2021@gmail.com>
2025-05-25 15:43:44 +0100

    setup shadcn with mono theme & add basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
 	import '../app.css';
 
+	import '@fontsource-variable/geist-mono';
+	import { ModeWatcher } from 'mode-watcher';
+
 	const { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher defaultMode="dark" />
+
+<div class="flex min-h-screen flex-col px-4 antialiased">
+	<div class="mx-auto flex w-full flex-grow items-start justify-center">
+		<div class="w-full max-w-3xl space-y-4">
+			<main class="flex flex-grow flex-col items-center justify-start gap-8">
+				{@render children()}
+			</main>
+		</div>
+	</div>
+</div>
